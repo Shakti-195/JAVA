1edd7b5e144c1352bd0ff48151ccac80c198e205
Shakti_Singh_@195 <147183484+Shakti-195@users.noreply.github.com>
2023-11-25 21:21:37 +0530

    Update

---
+++ Spiritual.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class spiritual{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+System.out.println ("Enter the Number:");
+int a =sc.nextInt();
+if(a>=0){
+    System.out.println("You are Astik"+" "+"Jai Shri Ram /Jai Shri Hanuman");
+}
+else if(a<0){
+    System.out.println("You are Nastik"+" "+"Go to Hell");
+    }
+  }
+}
+
+
+output:
+Enter the Number:
+1
+You are Astik Jai Shri Ram /Jai Shri Hanuman
+
+Enter the Number:
+-1
+You are Nastik Go to Hell
