852c6e8605b883fd18547ef2d7d74eeb9bd31d13
Shakti_Singh_@195 <147183484+Shakti-195@users.noreply.github.com>
2023-11-25 19:19:40 +0530

    Update Readkb.java

--- Readkb.java
+++ Readkb.java
@@ -1,1 +1,24 @@
-http://tpcg.io/WZ7C6Z
+import java.util.*;
+import java.util.*;
+
+public class ReadKb{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        
+        int X,Y;
+        System.out.println("Enter 2 Numbers");
+        X=sc.nextInt();
+        Y=sc.nextInt();
+        
+        int z=X+Y;
+        System.out.println("Sum is"+" "+z);
+        
+        
+    }
+}
+
+Output:-
+Enter 2 Numbers
+24
+25
+Sum is 49
