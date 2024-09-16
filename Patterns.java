1bf2cb9da981b618be49211def01a243422e7518
Shakti_Singh_@195 <147183484+Shakti-195@users.noreply.github.com>
2024-09-16 11:32:37 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -39,3 +39,22 @@ public class Main{
     }
   }
 }
+
+//Solid Rhombus
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    int n=5;
+    for(int i=1;i<=n;i++){
+      //spaces
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      //Stars
+      for(int j=1;j<=5;j++){
+        System.out.print("*");
+         }
+        System.out.println();
+      }
+    }
+ 
