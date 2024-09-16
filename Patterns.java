19ace27a45b6fb2702ef4139797ddfef1fc41c43
Shakti_Singh_@195 <147183484+Shakti-195@users.noreply.github.com>
2024-09-16 11:56:27 +0530

    Update Patterns.java
    Advance patterns

--- Patterns.java
+++ Patterns.java
@@ -57,4 +57,77 @@ public class Main{
         System.out.println();
       }
     }
- 
+
+  //Numbers Pyramid
+  import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    int n=5;
+    for(int i=1;i<=n;i++){
+      //spaces
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      //numbers->print row no,row no.times
+      for(int j=1;j<=i;j++){
+        System.out.print(i+" ");
+         }
+        System.out.println();
+      }
+    }
+}
+
+//Palindromic Pattern
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    int n=5;
+    for(int i=1;i<=n;i++){
+      //spaces
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      //1st half numbers
+      for(int j=i;j>=1;j--){
+        System.out.print(j);
+         }
+      //2nd half numbers
+      for(int j=2;j<=i;j++){
+        System.out.print(j);
+      }
+        System.out.println();
+      }
+    }
+}
+
+//Diamond Pattern
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    int n=4;
+    //upper half
+    for(int i=1;i<=n;i++){
+      //spaces
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      //Stars
+      for(int j=1;j<=2*i-1;j++){
+        System.out.print("*");
+         }
+       System.out.println();
+      }
+      //lower half
+    for(int i=n;i>=1;i--){
+      //Spaces
+      for(int j=1;j<=n-i;j++){
+        System.out.print(" ");
+      }
+      //Stars
+      for(int j=1;j<=2*i-1;j++){
+      System.out.print("*");
+    }
+    System.out.println();
+      }
+   }
+}
